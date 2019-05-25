0ff9e17356f1465bb5cd9a68aaa17587d65c27b8
Elie Mugenzi <eliemugenzi@Elies-MacBook-Pro.local>
2019-05-25 19:40:34 +0200

    Return userdata for log in

--- server/controllers/Auth.controller.js
+++ server/controllers/Auth.controller.js
@@ -73,7 +73,7 @@ class AuthController {
                     jwt.sign(currentUser, process.env.SECRET_KEY, (err, token) => {
                         res.status(200).json({
                             status: 200,
-                            data: [{ token }],
+                            data: [{ token, user: currentUser }],
                         });
                     });
                 } else {
