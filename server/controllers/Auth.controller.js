ce7ab8e44c50fcbaf1c593b8fbba4e91c412a5d1
Elie Mugenzi <eliemugenzi@Elies-MacBook-Pro.local>
2019-05-25 16:12:20 +0200

    Fix registration error

--- server/controllers/Auth.controller.js
+++ server/controllers/Auth.controller.js
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 import jwt from "jsonwebtoken";
 import User from "../models/User.model";
 
@@ -7,16 +8,26 @@ class AuthController {
             email, firstname, lastname, password,
         } = req.body;
 
-        const newUser = {
-            email, firstname, lastname, password, type: "client", isAdmin: false,
-        };
-        new User(newUser).save().then((user) => {
-            jwt.sign(user, process.env.SECRET_KEY, (err, token) => {
-                res.json({
-                    status: 201,
-                    message: "New User Created!",
-                    data: [{ token }],
-                }).status(201);
+        // eslint-disable-next-line consistent-return
+        User.findByEmail({ email }).then((thatUser) => {
+            if (Object.keys(thatUser).length) {
+                return res.status(400).json({
+                    status: 400,
+                    error: "User with this email already exists",
+                });
+            }
+
+            const newUser = {
+                email, firstname, lastname, password, type: "client", isAdmin: false,
+            };
+            new User(newUser).save().then((user) => {
+                jwt.sign(user, process.env.SECRET_KEY, (err, token) => {
+                    res.json({
+                        status: 201,
+                        message: "New User Created!",
+                        data: [{ token }],
+                    }).status(201);
+                });
             });
         });
     }
@@ -34,6 +45,14 @@ class AuthController {
             isAdmin: false,
             verified: false,
         };
+        User.findByEmail({ email }).then((thatUser) => {
+            if (Object.keys(thatUser).length) {
+                return res.status(400).json({
+                    status: 400,
+                    error: "User with that email already exists!",
+                });
+            }
+        });
         new User(newUser).save().then((user) => {
             jwt.sign(user, process.env.SECRET_KEY, (err, token) => {
                 res.status(201).json({
